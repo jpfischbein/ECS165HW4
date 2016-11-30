9432d902ad5380341a40456034ce48127ff64337
ImJohnDoe <Pshichenko@hotmail.com>
2016-11-29 23:42:21 -0800

    Initial commit to eclipse migration. (#5)

---
+++ src/exiforjson/EXIforJSONEncoder.c
@@ -0,0 +1,501 @@
+/*
+ * Copyright (C) 2007-2016 Siemens AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "EXIforJSONEncoder.h"
+
+#include "../json/jsmn.h"
+#include "../codec/EXITypes.h"
+#include "../codec/EXIforJSONQNames.h"
+#include "../codec/StringNameTable.h"
+#include "../codec/EXIforJSONEXIEncoder.h"
+
+#include "../codec/StringValueTable.h"
+#include "../codec/DynamicMemory.h"
+
+#ifndef EXI_FOR_JSON_ENCODER_C
+#define EXI_FOR_JSON_ENCODER_C
+
+/** EXI Debug mode */
+#define EXI_ENCODE_DEBUG 0
+
+#if EXI_ENCODE_DEBUG == 1
+# define DEBUG_PRINTF(x) printf x
+#else
+# define DEBUG_PRINTF(x) do {} while (0)
+#endif
+
+static uint16_t numberOfLocalStringsEncode[EXI_EXIforJSONMAX_NUMBER_OF_QNAMES];
+
+static exi_value_t val;
+
+static int dumpFloat(const char *js, int start, int end, bitstream_t* stream,
+		exi_state_t* state) {
+	int errn;
+	/* minus */
+	/* dot */
+	/* E/e */
+	int pos = start;
+	int negative = 0;
+	int negativeE = 0;
+	int mantissa = 0;
+	int exponent = 0;
+	int exponentE = 0;
+	int afterDot = 0;
+	int afterE = 0;
+
+	while (pos < end) {
+		if (js[pos] == '-') {
+			if(afterE) {
+				negativeE = 1;
+			} else {
+				negative = 1;
+			}
+		} else if (js[pos] == '+') {
+			/* */
+		} else if (js[pos] == '.') {
+			afterDot = 1;
+		} else if (js[pos] == 'E' || js[pos] == 'e') {
+			afterE = 1;
+		} else if ((js[pos] >= '0') && (js[pos] <= '9')) {
+			if(afterE) {
+				exponentE = (exponentE * 10) + ((js[pos]) - '0');
+			} else {
+				mantissa = (mantissa * 10) + ((js[pos]) - '0');
+			}
+			if (afterDot && !afterE) {
+				exponent -= 1;
+			}
+		}
+		pos++;
+	}
+
+	if (negativeE) {
+		exponentE = exponentE * -1;
+	}
+	exponent = exponent + exponentE;
+	if (negative) {
+		mantissa = mantissa * -1;
+	}
+
+	/* print number */
+	DEBUG_PRINTF(("float M:%d, E:%d ", mantissa, exponent));
+
+	val.type = EXI_DATATYPE_FLOAT;
+	val.float_me.mantissa = mantissa;
+	val.float_me.exponent = exponent;
+	errn = exiEXIforJSONEncodeCharacters(stream, state, &val);
+
+	return errn;
+}
+
+static int pendingKeyStart;
+static int pendingKeyEnd;
+
+static int dumpKey(const char *js, jsmntok_t *t, bitstream_t* stream,
+		exi_state_t* state) {
+	if (t->type == JSMN_STRING) {
+		DEBUG_PRINTF(("#%.*s#", t->end - t->start, js+t->start));
+		pendingKeyStart = t->start;
+		pendingKeyEnd = t->end;
+	} else {
+		/* error */
+		DEBUG_PRINTF(("Error: no key \n"));
+	}
+
+	return 1;
+}
+
+static int updateStringValue(const char *s, uint16_t start, uint16_t end,
+		exi_value_table_t* valueTable, uint16_t qnameID) {
+	int errn = 0;
+	uint16_t slen = end - start;
+	int i, k;
+	exi_value_string_table_entry_t* strHit = NULL;
+	uint32_t globalID;
+
+	if (valueTable->numberOfGlobalStrings > 0) {
+		for (i = 0; i < valueTable->valueStringTable->len && strHit == NULL;
+				i++) {
+			if (valueTable->valueStringTable->strs[i].str.len == slen) {
+				/* compare characters first by length */
+				int match = 1;
+				for (k = 0; k < slen && match; k++) {
+					if (valueTable->valueStringTable->strs[i].str.characters[k]
+							!= s[start + k]) {
+						/* mis-match */
+						match = 0;
+					}
+				}
+				if (match) {
+					strHit = &valueTable->valueStringTable->strs[i];
+					globalID = i;
+				}
+			}
+		}
+	}
+
+	val.type = EXI_DATATYPE_STRING;
+	if (strHit == NULL) {
+		/* Miss */
+		val.str.type = EXI_STRING_VALUE_MISS;
+		val.str.miss.len = slen;
+		val.str.miss.size = val.str.miss.len;
+		val.str.miss.characters = (exi_string_character_t*) &s[start];
+	} else {
+		/* Hit */
+		if (qnameID == strHit->qnameID) {
+			/* local hit */
+			val.str.type = EXI_STRING_VALUE_LOCAL_HIT;
+			val.str.localID = strHit->localValueID;
+		} else {
+			/* global hit */
+			val.str.type = EXI_STRING_VALUE_GLOBAL_HIT;
+			val.str.globalID = globalID;
+		}
+		val.str.miss.len = slen;
+		val.str.miss.size = val.str.miss.len;
+		val.str.miss.characters = (exi_string_character_t*) &s[start];
+	}
+
+	return errn;
+}
+
+static int checkPendingKey(const char *js, bitstream_t* stream,
+		exi_state_t* state) {
+	int errn = 0;
+
+	if (pendingKeyStart > 0) {
+		errn = updateStringValue(js, pendingKeyStart, pendingKeyEnd,
+				state->stringTable, EXI_EXIforJSON_0_key);
+		errn = exiEXIforJSONEncodeAttribute(stream, state, EXI_EXIforJSON_0_key,
+				&val);
+
+		pendingKeyStart = 0; /* signals no pending key */
+	}
+	return errn;
+}
+
+/*
+ * An example of reading JSON and printing its content
+ */
+
+static int dump_loop(const char *js, jsmntok_t *t_, bitstream_t* stream,
+		exi_state_t* state) {
+
+	typedef struct jsmn_stack {
+		jsmntok_t * token;
+		int loop_index;
+		struct jsmn_stack * parent;
+	} jsmn_stack_t;
+
+	jsmn_stack_t root = { 0 };
+	root.parent = 0;
+	root.loop_index = 0;
+	root.token = &t_[0];
+
+	jsmn_stack_t * stack = &root;
+
+	int errn = 0;
+	int j = 0;
+
+	while (1) {
+
+		/* If we have an empty object or array at root we need special handling */
+		if (stack->parent == 0 && stack->token->size == 0) {
+
+			switch (stack->token->type) {
+			case JSMN_ARRAY:
+				errn = exiEXIforJSONEncodeStartElement(stream, state,
+						EXI_EXIforJSON_4_array);
+				break;
+			case JSMN_OBJECT:
+				errn = exiEXIforJSONEncodeStartElement(stream, state,
+						EXI_EXIforJSON_4_map);
+				break;
+			default:
+				/* avoid warnings enumeration value 'FOO' not handled in switch */
+				break;
+			}
+
+			errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(???) */
+		}
+
+		for (; stack->loop_index < stack->token->size;
+				stack->loop_index++, j++) {
+
+			/* Root Object has no key */
+			if (stack->parent && stack->token->type == JSMN_OBJECT) {
+				j += dumpKey(js, &t_[j], stream, state);
+			}
+
+			jsmntok_t * t = &t_[j];
+
+			switch (t->type) {
+			case JSMN_PRIMITIVE:
+				/* number, true, false, or null */
+				if (js[t->start] == 't') {
+					errn = exiEXIforJSONEncodeStartElement(stream, state,
+							EXI_EXIforJSON_4_boolean);
+
+					checkPendingKey(js, stream, state);
+					DEBUG_PRINTF(("booleanTrue \n"));
+					val.type = EXI_DATATYPE_BOOLEAN;
+					val.boolean = 1;
+					errn = exiEXIforJSONEncodeCharacters(stream, state, &val);
+					errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(boolean) */
+				} else if (js[t->start] == 'f') {
+					errn = exiEXIforJSONEncodeStartElement(stream, state,
+							EXI_EXIforJSON_4_boolean);
+
+					checkPendingKey(js, stream, state);
+					DEBUG_PRINTF(("booleanFalse \n"));
+					val.type = EXI_DATATYPE_BOOLEAN;
+					val.boolean = 0;
+					errn = exiEXIforJSONEncodeCharacters(stream, state, &val);
+					errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(boolean) */
+				} else if (js[t->start] == 'n') {
+					errn = exiEXIforJSONEncodeStartElement(stream, state,
+							EXI_EXIforJSON_4_null);
+
+					checkPendingKey(js, stream, state);
+					DEBUG_PRINTF(("NULL \n"));
+					errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(null) */
+				} else {
+					errn = exiEXIforJSONEncodeStartElement(stream, state,
+							EXI_EXIforJSON_4_number);
+
+					checkPendingKey(js, stream, state);
+					/* printf("NUMBER %d %d \n", t->start, t->end); */
+					/* printf("%.*s", t->end - t->start, js+t->start); */
+					dumpFloat(js, t->start, t->end, stream, state);
+					errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(number) */
+				}
+				/* printf("%.*s", t->end - t->start, js+t->start); */
+				break;
+			case JSMN_STRING:
+				errn = exiEXIforJSONEncodeStartElement(stream, state,
+						EXI_EXIforJSON_4_string);
+
+				checkPendingKey(js, stream, state);
+				DEBUG_PRINTF(("'%.*s'", t->end - t->start, js + t->start));
+
+				errn = updateStringValue(js, t->start, t->end,
+						state->stringTable, EXI_EXIforJSON_4_string);
+				errn = exiEXIforJSONEncodeCharacters(stream, state, &val);
+				errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(string) */
+				break;
+			case JSMN_OBJECT:
+				errn = exiEXIforJSONEncodeStartElement(stream, state,
+						EXI_EXIforJSON_4_map);
+
+				checkPendingKey(js, stream, state);
+				DEBUG_PRINTF(("{\n"));
+
+				if (errn == 0) {
+
+					jsmn_stack_t * descend = (jsmn_stack_t*) malloc(
+							sizeof(jsmn_stack_t));
+					/* TODO NULL check */
+					descend->parent = stack;
+					descend->loop_index = -1; /* will be increased before use by the for-loop */
+					descend->token = t;
+
+					if (descend->token->type == JSMN_PRIMITIVE) {
+						descend->token->size = 1;
+					}
+
+					stack->loop_index++; /* make sure to advance the parent element before descendin*/
+					stack = descend;
+
+					/* end element will be encoded later on when we leave the lower stack level */
+					if (errn != 0) {
+						DEBUG_PRINTF(("EXI Error: end object\n"));
+					}
+				} else {
+					DEBUG_PRINTF(("EXI Error: start object\n"));
+				}
+				break;
+			case JSMN_ARRAY:
+				errn = exiEXIforJSONEncodeStartElement(stream, state,
+						EXI_EXIforJSON_4_array);
+
+				checkPendingKey(js, stream, state);
+
+				jsmn_stack_t * descend = (jsmn_stack_t*) malloc(
+						sizeof(jsmn_stack_t));
+				/* TODO NULL check */
+				descend->parent = stack;
+				descend->loop_index = -1; /* will be increased before use by the for-loop */
+				descend->token = t;
+
+				if (descend->token->type == JSMN_PRIMITIVE) {
+					descend->token->size = 1;
+				}
+
+				stack->loop_index++; /* make sure to advance the parent element before descending */
+				stack = descend;
+
+				DEBUG_PRINTF(("[\n"));
+				break;
+			default:
+				/* avoid warnings enumeration value 'FOO' not handled in switch */
+				break;
+			}
+		} /* for */
+
+		/* if we did not complete this object / array do not delete, yet */
+		if (stack->loop_index < stack->token->size) {
+			continue;
+		}
+
+		/* If we went down a level go up again and delete our helper struct from the heap */
+		if (stack->parent) {
+			switch (stack->token->type) {
+			case JSMN_ARRAY:
+			case JSMN_OBJECT:
+				errn = exiEXIforJSONEncodeEndElement(stream, state); /* EE(???) */
+				DEBUG_PRINTF(("(]})\n"));
+				break;
+			default:
+				/* avoid warnings enumeration value 'FOO' not handled in switch */
+				break;
+			}
+
+			jsmn_stack_t * temp = stack->parent;
+
+			free(stack);
+
+			stack = temp;
+		} else {
+			/* We are back the root element and done */
+			break;
+		}
+	} /* while(1) */
+
+	return errn;
+}
+
+int encodeEXIforJSONsharedStrings(const char *json, size_t jlen,
+		uint8_t* buffer, size_t blen, size_t* posEncode,
+		const char ** sharedStrings, size_t stlen) {
+	int r, i;
+	int errn = 0;
+	jsmn_parser p;
+
+	pendingKeyStart = 0;
+
+	/* EXI encoder setup*/
+	bitstream_t oStream;
+	exi_name_table_runtime_t runtimeTableEncode;
+	exi_state_t stateEncode;
+	exi_value_table_t stringTableEncode =
+			{ 0, EXI_EXIforJSONMAX_NUMBER_OF_QNAMES, numberOfLocalStringsEncode,
+					NULL };
+
+	/** string values */
+	exi_value_string_table_t stringTableValuesEncode;
+	stringTableValuesEncode.size = 0;
+	stringTableValuesEncode.len = 0;
+	stringTableValuesEncode.strs = 0;
+	stringTableEncode.valueStringTable = &stringTableValuesEncode;
+
+	/* BINARY memory setup */
+	/* val.binary.len = 0;
+	 val.binary.size = 0;
+	 val.binary.data = NULL;*/
+
+	/* STRING miss memory setup */
+	/* val.str.type = EXI_STRING_VALUE_MISS;
+	 val.str.miss.len = 0;
+	 val.str.miss.size = 0;
+	 val.str.miss.characters = NULL; */
+
+	/* setup output stream */
+	oStream.size = blen;
+	oStream.data = buffer;
+	oStream.pos = (uint16_t*) posEncode;
+
+	/* init encoder (write header, set initial state) */
+	errn = exiInitNameTableRuntime(&runtimeTableEncode);
+	if (errn == 0) {
+		errn = exiEXIforJSONInitEncoder(&oStream, &stateEncode,
+				&runtimeTableEncode, &stringTableEncode);
+
+		if (errn == 0 && stlen > 0 && sharedStrings != NULL) {
+			exi_string_t sv;
+			for (i = 0; i < stlen; i++) {
+				sv.characters = (exi_string_character_t*) sharedStrings[i];
+				sv.len = sv.size = strlen(sharedStrings[i]);
+				errn = exiAddStringValue(stateEncode.stringTable, &sv,
+						EXI_EXIforJSON_2_nil);
+			}
+		}
+	}
+
+	const int jsmnTokens = 128;
+	jsmntok_t * t = (jsmntok_t*) malloc(jsmnTokens * sizeof(jsmntok_t));
+
+	jsmn_init(&p);
+	r = jsmn_parse(&p, json, strlen(json), t, jsmnTokens);
+
+	if (r < 0) {
+		DEBUG_PRINTF(("Failed to parse JSON: %d\n", r));
+		errn = 1;
+	} else {
+		errn = exiEXIforJSONEncodeStartDocument(&oStream, &stateEncode);
+		if (errn == 0) {
+			dump_loop(json, t, &oStream, &stateEncode);
+			errn = exiEXIforJSONEncodeEndDocument(&oStream, &stateEncode);
+		}
+
+		if (errn) {
+			DEBUG_PRINTF(("[Encode-ERROR] %d \n", errn));
+		}
+	}
+
+	free(t);
+	t = 0;
+
+	/* free memory if any */
+	/* exiFreeDynamicStringMemory(&val.str.miss);*//* val.str.miss points to passed JSON string */
+	/* exiFreeDynamicBinaryMemory(&val.binary);*//* binary not used */
+	for (i = stlen; i < stringTableValuesEncode.len; i++) {
+		exiFreeDynamicStringMemory(&stringTableValuesEncode.strs[i].str);
+	}
+	if (stringTableValuesEncode.strs) {
+		free(stringTableValuesEncode.strs);
+		stringTableValuesEncode.strs = 0;
+	}
+
+	return errn;
+}
+
+int encodeEXIforJSON(const char *json, size_t jlen, uint8_t* buffer,
+		size_t blen, size_t* posEncode) {
+	return encodeEXIforJSONsharedStrings(json, jlen, buffer, blen, posEncode,
+			NULL, 0);
+}
+
+#endif
---
+++ src/test/main.c
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2007-2016 Siemens AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*******************************************************************
+ *
+ * @author Daniel.Peintner.EXT@siemens.com
+ * @contact Joerg.Heuer@siemens.com
+ *
+ * <p>Sample program</p>
+ *
+ ********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../exiforjson/EXIforJSONEncoder.h"
+#include "../exiforjson/EXIforJSONDecoder.h"
+#include "../json/jsmn.h"
+
+#define BUFFER_SIZE 2048
+
+/* ========== JSON SAMPLES */
+#define SAMPLE0 "{\"float1\": 1.1e500, \"float2\": -12.34, \"float1\": -23.45E-7}"
+/* */
+#define SAMPLE1 "{\"keyNumber\":   123, \"k\":-12.34, \"keyArrayStrings\": [ \"s1\", \"s2\" ] , \"valid\": true, \"foo\": null}"
+/* test01.json */
+#define SAMPLE2 "{\"keyNumber\":123,\"keyArrayStrings\":[\"s1\",\"s2\"]}"
+/* String Hits (local/global) */
+#define SAMPLE3 "{\"key1\":\"s1\",\"keyArrayStrings\":[\"s1\",\"s2\"],\"s1\":null} "
+/* http://json.org/example.html #1 */
+#define SAMPLE4 "{\"glossary\":{\"title\":\"example glossary\",\"GlossDiv\":{\"title\":\"S\",\"GlossList\":{\"GlossEntry\":{\"ID\":\"SGML\",\"SortAs\":\"SGML\",\"GlossTerm\":\"Standard Generalized Markup Language\",\"Acronym\":\"SGML\",\"Abbrev\":\"ISO 8879:1986\",\"GlossDef\":{\"para\":\"A meta-markup language, used to create markup languages such as DocBook.\",\"GlossSeeAlso\":[\"GML\",\"XML\"]},\"GlossSee\":\"markup\"}}}}} "
+/* http://json.org/example.html #2 */
+#define SAMPLE5 "{\"menu\":{\"id\":\"file\",\"value\":\"File\",\"popup\":{\"menuitem\":[{\"value\":\"New\",\"onclick\":\"CreateNewDoc()\"},{\"value\":\"Open\",\"onclick\":\"OpenDoc()\"},{\"value\":\"Close\",\"onclick\":\"CloseDoc()\"}]}}} "
+/* http://json.org/example.html #3 */
+#define SAMPLE6 "{\"widget\":{\"debug\":\"on\",\"window\":{\"title\":\"Sample Konfabulator Widget\",\"name\":\"main_window\",\"width\":500,\"height\":500},\"image\":{\"src\":\"Images/Sun.png\",\"name\":\"sun1\",\"hOffset\":250,\"vOffset\":250,\"alignment\":\"center\"},\"text\":{\"data\":\"Click Here\",\"size\":36,\"style\":\"bold\",\"name\":\"text1\",\"hOffset\":250,\"vOffset\":100,\"alignment\":\"center\",\"onMouseUp\":\"sun1.opacity = (sun1.opacity / 100) * 90;\"}}} "
+/* https://github.com/w3c/wot/blob/master/TF-TD/TD%20Samples/led.jsonld */
+#define SAMPLE7 "{\"@context\":\"http://w3c.github.io/wot/w3c-wot-td-context.jsonld\",\"metadata\":{\"name\":\"MyLED\",\"protocols\":{\"CoAP\":{\"uri\":\"coap://www.example.com:5683/ledlamp\",\"priority\":1},\"HTTP\":{\"uri\":\"http://www.example.com:80/ledlamp\",\"priority\":2}},\"encodings\":[\"JSON\"]},\"interactions\":[{\"@type\":\"Property\",\"name\":\"colorTemperature\",\"outputData\":\"xsd:unsignedShort\",\"writable\":true},{\"@type\":\"Property\",\"name\":\"rgbValueRed\",\"outputData\":\"xsd:unsignedByte\",\"writable\":false},{\"@type\":\"Property\",\"name\":\"rgbValueGreen\",\"outputData\":\"xsd:unsignedByte\",\"writable\":false},{\"@type\":\"Property\",\"name\":\"rgbValueBlue\",\"outputData\":\"xsd:unsignedByte\",\"writable\":false},{\"@type\":\"Action\",\"name\":\"ledOnOff\",\"inputData\":\"xsd:boolean\",\"outputData\":\"\"},{\"@type\":\"Action\",\"name\":\"fadeIn\",\"inputData\":\"xsd:unsignedByte\",\"outputData\":\"\"},{\"@type\":\"Action\",\"name\":\"fadeOut\",\"inputData\":\"xsd:unsignedByte\",\"outputData\":\"\"},{\"@type\":\"Event\",\"outputData\":\"xsd:unsignedShort\",\"name\":\"colorTemperatureChanged\"}]}"
+/* ==========  */
+
+/* ========== SHARED STRINGS may be used to establish a shared context between encoder & decoder
+ * ========== and allows for increased processing and smaller stream size */
+const char *SHARED_STRINGS[] = {"@context","@id","@value","@type","xsd:string","xsd:boolean","xsd:unsignedShort","xsd:unsignedByte", "xsd:float","http://w3c.github.io/wot/w3c-wot-td-context.jsonld","metadata","name","protocols","uri","priority","encodings","interactions","outputData","inputData","writable","Property","Action","Event","CoAP","HTTP","JSON","EXI", "WS"};
+const int NUMBER_OF_SHARED_STRINGS = sizeof(SHARED_STRINGS)/sizeof(SHARED_STRINGS[0]);
+/* ==========  */
+
+
+static int test(const char* JSON_STRING_IN, const int useSharedStrings) {
+	int errn = 0;
+
+	const size_t lenOut = strlen(JSON_STRING_IN) + 100; /* some extra space for decoding differences e.g, number 1 -> 1E0 etc */
+	char JSON_STRING_OUT[lenOut];
+
+	uint8_t buffer[BUFFER_SIZE];
+	size_t posEncode = 0;
+	size_t posDecode = 0;
+
+	printf("FROM (%s): \n%s \n", useSharedStrings ? "withSharedStrings" : "withoutSharedStrings", JSON_STRING_IN);
+
+	if(useSharedStrings) {
+		errn = encodeEXIforJSONsharedStrings(JSON_STRING_IN, strlen(JSON_STRING_IN), buffer, BUFFER_SIZE, &posEncode, SHARED_STRINGS, NUMBER_OF_SHARED_STRINGS);
+	} else {
+		errn = encodeEXIforJSON(JSON_STRING_IN, strlen(JSON_STRING_IN), buffer, BUFFER_SIZE, &posEncode);
+	}
+	if( errn == 0 ) {
+		/* OK so far */
+		printf("Encoding JSON (len=%d) to EXIforJSON (len=%d) was successful \n", strlen(JSON_STRING_IN), posEncode);
+
+		/* Try to transform it back to JSON again */
+		if(useSharedStrings) {
+			errn = decodeEXIforJSONsharedStrings(buffer, BUFFER_SIZE, &posDecode, JSON_STRING_OUT, lenOut, SHARED_STRINGS, NUMBER_OF_SHARED_STRINGS);
+		} else {
+			errn = decodeEXIforJSON(buffer, BUFFER_SIZE, &posDecode, JSON_STRING_OUT, lenOut);
+		}
+
+		if( errn == 0 ) {
+			/* OK */
+			printf("Decoding EXIforJSON (len=%d) to JSON (len=%d) was successful \n", posEncode, strlen(JSON_STRING_OUT));
+			printf("TO (%s): \n%s \n", useSharedStrings ? "withSharedStrings" : "withoutSharedStrings", JSON_STRING_OUT);
+
+			/* parse JSON again to check syntax */
+			const int jsmnTokens = 128;
+			jsmntok_t * t = (jsmntok_t*) malloc(jsmnTokens * sizeof(jsmntok_t));
+
+			jsmn_parser p;
+			jsmn_init(&p);
+			int cnt  = jsmn_parse(&p, JSON_STRING_OUT, strlen(JSON_STRING_OUT), t, jsmnTokens);
+			if(cnt < 0) {
+				/* error */
+				errn = cnt;
+				printf("\tParsing JSON failed! \n");
+			}
+			/* TODO JSON compare */
+		} else {
+			/* ERROR */
+			printf("Decoding EXIforJSON to JSON failed due to error %d \n", errn);
+		}
+	} else {
+		/* ERROR */
+		printf("Encoding JSON to EXIforJSON failed due to error %d \n", errn);
+	}
+
+	printf("- - - \n");
+
+	return errn;
+}
+
+int main(int argc, char *argv[]) {
+	int errn = 0;
+
+	char *JSON_STRING_IN;
+
+	/* test various JSON inputs with/without shared strings */
+
+	/* SAMPLE0 */
+	JSON_STRING_IN = SAMPLE0;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE1 */
+	JSON_STRING_IN = SAMPLE1;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE2 */
+	JSON_STRING_IN = SAMPLE2;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE3 */
+	JSON_STRING_IN = SAMPLE3;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE4 */
+	JSON_STRING_IN = SAMPLE4;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE5 */
+	JSON_STRING_IN = SAMPLE5;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE6 */
+	JSON_STRING_IN = SAMPLE6;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	/* SAMPLE7 */
+	JSON_STRING_IN = SAMPLE7;
+	errn = test(JSON_STRING_IN, 0);
+	if (errn) return errn;
+	errn = test(JSON_STRING_IN, 1);
+	if (errn) return errn;
+
+	return errn;
+}
+
